77b058393e40b78ff492f5cf2ec5794b6ddaec67
Montez-Hill <tezzy823@gmail.com>
2025-09-27 22:46:17 -0400

    Player character and movement

---
+++ KB Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Speed of movement
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        // Get Rigidbody2D and Animator components
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Get player input using arrow keys
+        moveInput.x = Input.GetAxisRaw("Horizontal"); // Left/Right arrows
+        moveInput.y = Input.GetAxisRaw("Vertical");   // Up/Down arrows
+
+        // Normalize so diagonal movement isn't faster
+        moveInput = moveInput.normalized;
+
+        // Update animator parameters if you have running animations
+        if (animator != null)
+        {
+            animator.SetFloat("MoveX", moveInput.x);
+            animator.SetFloat("MoveY", moveInput.y);
+            animator.SetFloat("Speed", moveInput.sqrMagnitude);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Apply movement using Rigidbody2D
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+}
