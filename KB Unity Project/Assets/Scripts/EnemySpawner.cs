96c69690b9ff87df9984450c3fd0aa3a03024fb0
jc1748 <156351014+jc1748@users.noreply.github.com>
2025-10-14 12:45:06 -0400

    bug fixes

--- KB Unity Project/Assets/Scripts/EnemySpawner.cs
+++ KB Unity Project/Assets/Scripts/EnemySpawner.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject Prototype2;
+    public GameObject Shooter;
     public Transform spawnPoint;
     public int maxEnemies = 3;
     public float spawnDelay = 0.5f;
